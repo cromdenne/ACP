40a827851b25a2419924d9e376e904845cf1ec71
Cory Romdenne <cory@romdenne.com>
2018-01-11 10:11:04 -0600

    Embed Twitter feed
    Added new ACP Twitter feed to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,21 @@
 <div id="footer" class="row">
                 <div id="footer-left" class="col-md-4">
                     <address>
-                        LUXEMBURG-CASCO SCHOOL DISTRICT<br>
+                        <h5>LUXEMBURG-CASCO SCHOOL DISTRICT</h5>
                         318 N Main St &nbsp;|&nbsp; Luxemburg, WI 54217<br>
                         (920) 845-2391 &nbsp;|&nbsp; 
                         <a class="hvr" href="http://www.luxcasco.k12.wi.us" target="_blank">luxcasco.k12.wi.us</a><br>
                         &copy; 2017 Luxemburg-Casco School District
                     </address>
+                    <br>
+                    <h5>MISSION</h5>
+                    <p>Preparing our students to thrive in a global community by ensuring excellence in intellect and virtue. We are Spartans!</p>
+                    <br>
                 </div><!-- /.col-md-4 -->
                 <div id="footer-center" class="col-md-4 text-center">
                     <img src="images/LuxCascoSchoolLogo-RGB.png" alt="Luxemburg-Casco Schoool District logo" width="200px">
                 </div>
-                <div id="footer-right" class="col-md-3 col-md-offset-1 text-right">
-                    <p>MISSION<br>
-                    Preparing our students to thrive in a global community by ensuring excellence in intellect and virtue. We are Spartans!</p>
+                <div id="footer-right" class="col-md-4 text-right">
+                    <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/LuxCascoACP?ref_src=twsrc%5Etfw">Tweets by LuxCascoACP</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                 </div>
             </div><!-- /#footer -->
