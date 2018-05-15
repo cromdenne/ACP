f5018e616575e7a441b5cddeaabd0eab4e0ae45e
Cory Romdenne <cory@romdenne.com>
2018-05-15 10:04:19 -0500

    Add career fair image to go page

--- go.php
+++ go.php
@@ -1,130 +1,134 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Academic & Career Planning - Luxemburg-Casco School District">
+    <meta name="keywords" content="Luxemburg-Casco,Wisconsin,ACP,Academic,Career,Planning">
+    <meta name="author" content="Cory Romdenne">
     
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Academic & Career Planning - Luxemburg-Casco School District">
-        <meta name="keywords" content="Luxemburg-Casco,Wisconsin,ACP,Academic,Career,Planning">
-        <meta name="author" content="Cory Romdenne">
-        
-        <title>Go – L-C ACP</title>
-        
-        <?php include 'templates/head.php'; ?>
-            
-    </head>
+    <title>Go – L-C ACP</title>
     
-    <body>
-
-        <div class="container-fluid">
+    <?php include 'templates/head.php'; ?>
         
-            <?php include 'templates/mast-begin.php'; ?>
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="know.php">Know</a></li>
-                            <li><a href="explore.php">Explore</a></li>
-                            <li><a href="plan.php">Plan</a></li>
-                            <li class="active"><a href="go.php">Go <span class="sr-only">(current)</span></a></li>
-                            <li><a href="pi26.php">PI 26 &amp; ACP</a></li>
-                            <li><a href="parents.php">Parents</a></li>
-                            <li><a href="staff.php">Staff</a></li>
-                            <li><a href="partnerships.php">Partnerships</a></li>
-                <?php include 'templates/mast-end.php'; ?>
+  </head>
+  
+  <body>
 
-            <div id="content" class="row row-eq-height">
-                <div id="pane-left" class="col-md-9">
-                    <h1>Go</h1>
-                    <img src="images/spear.png" width="100%" alt="spear header underline">
-                    <br><br>
-                    <p>In "Go", L-C students take time to reflect on and update their personal, academic, and career goals. Students are encouraged to make modifications to their course outline as necessary to ensure their plan allows for a clear transition after high school into the following focuses: Armed Forces, Workforce, or Postsecondary Education.</p>
-                    <hr class="rule-light">
-                    <h3 class="fancy">Do it!</h3>
-                    <p>Carry out your plan and recalculate as needed. This stage includes the following topics.</p>
+    <div class="container-fluid">
+    
+      <?php include 'templates/mast-begin.php'; ?>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="know.php">Know</a></li>
+                <li><a href="explore.php">Explore</a></li>
+                <li><a href="plan.php">Plan</a></li>
+                <li class="active"><a href="go.php">Go <span class="sr-only">(current)</span></a></li>
+                <li><a href="pi26.php">PI 26 &amp; ACP</a></li>
+                <li><a href="parents.php">Parents</a></li>
+                <li><a href="staff.php">Staff</a></li>
+                <li><a href="partnerships.php">Partnerships</a></li>
+          <?php include 'templates/mast-end.php'; ?>
 
-                    <p class="graph-space">
-                        <h5>Execution of Plan</h5>
-                        <ul>
-                            <li>Students will participate in multiple career related-activities.</li>
-                            <li>Lead a student-parent-teacher conference.</li>
-                            <li>Seek out opportunities to better define their own career or postsecondary options.</li>
-                        </ul>
-                    </p>
+      <div id="content" class="row row-eq-height">
+        <div id="pane-left" class="col-md-9">
+          <h1>Go</h1>
+          <img src="images/spear.png" width="100%" alt="spear header underline">
+          <br><br>
+          <p>In "Go", L-C students take time to reflect on and update their personal, academic, and career goals. Students are encouraged to make modifications to their course outline as necessary to ensure their plan allows for a clear transition after high school into the following focuses: Armed Forces, Workforce, or Postsecondary Education.</p>
+          <hr class="rule-light">
+          <h3 class="fancy">Do it!</h3>
+          <p>Carry out your plan and recalculate as needed. This stage includes the following topics.</p>
 
-                    <p class="graph-space">
-                        <h5>Updating the Plan</h5>
-                        <ul>
-                            <li>Explore different career options within students areas of interests.</li>
-                            <li>Update plan regularly with academic and career related activities.</li>
-                            <li>Seek out and document activities completed in pursuit of goals.</li>
-                        </ul>
-                    </p>
+          <p class="graph-space">
+            <h5>Execution of Plan</h5>
+            <ul>
+              <li>Students will participate in multiple career related-activities.</li>
+              <li>Lead a student-parent-teacher conference.</li>
+              <li>Seek out opportunities to better define their own career or postsecondary options.</li>
+            </ul>
+          </p>
 
-                    <p class="graph-space">
-                        <h5>ACP Conferencing and Mentoring</h5>
-                        <ul>
-                            <li>Students will participate in regularly scheduled conferences to explore options and plan for opportunities to meet goals.</li>
-                            <li>Identify, document and seek out activities as desired to complete their plan.</li>
-                        </ul>
-                    </p>
+          <p class="graph-space">
+            <h5>Updating the Plan</h5>
+            <ul>
+              <li>Explore different career options within students areas of interests.</li>
+              <li>Update plan regularly with academic and career related activities.</li>
+              <li>Seek out and document activities completed in pursuit of goals.</li>
+            </ul>
+          </p>
 
-                    <p class="graph-space">
-                        <h5>Transitioning</h5>
-                        <ul>
-                            <li>Attend post-secondary and high school information events.</li>
-                            <li>Understand the value of persistence, practicality, and patience in gaining admission/employment.</li>
-                            <li>Use personal or school networks to gather leads, connections, and letters of recommendations.</li>
-                        </ul>
-                    </p>
+          <p class="graph-space">
+            <h5>ACP Conferencing and Mentoring</h5>
+            <ul>
+              <li>Students will participate in regularly scheduled conferences to explore options and plan for opportunities to meet goals.</li>
+              <li>Identify, document and seek out activities as desired to complete their plan.</li>
+            </ul>
+          </p>
 
-                </div><!-- /#main -->
-                <div id="pane-right" class="col-md-3 sidebar">
-                    <h3>Resources</h3>
-                    <ul class="unstyled">
-                        <li>
-                            <a class="hvr" href="https://drive.google.com/open?id=1oo56yREd9s_Zmw_sfa8bJ3xJs6jE-eb3" target="_blank">ACP Portfolio Requirements</a>
-                            <p class="list-graph">The ACP portfolio requirements allow students to reflect on their high school experiences through the lens of know, explore, plan, and go. This process helps students discover and develop a personalized plan for their life after high school.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://www.bls.gov/k12/" target="_blank">Bureau of Labor Statistics</a>
-                            <p class="list-graph">The Bureau of Labor Statistics’ mission is to collect, analyze, and disseminate essential economic information to support public and private decision-making. As an independent statistical agency, BLS serves its diverse user communities by providing products and services that are objective, timely, accurate, and relevant.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="http://dwd.wi.gov/skillexplorer/brochure.pdf" target="_blank">Skill Explorer</a>
-                            <p class="list-graph">Skill Explorer helps students identify transferable skills, career pathway opportunities, and training needs for occupations they are interested in pursuing.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="http://www.military.com/join-armed-forces/us-military-overview.html" target="_blank">U.S. Armed Forces Overview</a>
-                            <p class="list-graph">This link gives background of the Armed Forces and what they have to offer to people interested in joining, as well as news and information for active military and veterans.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://jobcenterofwisconsin.com" target="_blank">Job Center of Wisconsin</a>
-                            <p class="list-graph">This link allows employers and job seekers to search the state of Wisconsin for suitable employees and occupations.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="http://www.wtcsystem.edu/" target="_blank">Wisconsin Technical College System</a>
-                            <p class="list-graph">The Wisconsin Technical College System develops individuals at every stage of their academic and work careers into experts and entrepreneurs who make an immediate, local economic impact.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="http://www.wisconsinsprivatecolleges.org" target="_blank">Wisconsin Private Colleges</a>
-                            <p class="list-graph">Wisconsin's private, nonprofit colleges and universities working together for educational opportunity.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://www.wisconsin.edu" target="_blank">University of Wisconsin Colleges</a>
-                            <p class="list-graph">The UW System is made up of 13 four-year universities, 13 freshman-sophomore UW Colleges campuses, and statewide UW-Extension. Together, these institutions are a tremendous academic, cultural, and economic resource for Wisconsin, the nation, and the world.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://sso.careercruising.com/page/openidconnect" target="_blank">Career Cruising</a>
-                            <p class="list-graph">Career Cruising is an Internet-based career exploration and planning tool used by students in grades 6-12 to explore career and college options and develop a career plan. Through Career Cruising students will build self-awareness, learn about career possibilities and educational pathways, and create an actionable plan to achieve career, school and life goals.</p>
-                        </li>
-                    </ul>
-                </div><!-- /#sidebar -->
+          <p class="graph-space">
+            <h5>Transitioning</h5>
+            <ul>
+              <li>Attend post-secondary and high school information events.</li>
+              <li>Understand the value of persistence, practicality, and patience in gaining admission/employment.</li>
+              <li>Use personal or school networks to gather leads, connections, and letters of recommendations.</li>
+            </ul>
+          </p>
+          <div class="row">
+            <img class="img-responsive img-thumbnail"
+                 src="images/CareerFair2018.jpg"
+                 alt="Luxemburg-Casco Career Fair 2018">
+          </div><!-- /.row -->
+        </div><!-- /#main -->
+        <div id="pane-right" class="col-md-3 sidebar">
+          <h3>Resources</h3>
+          <ul class="unstyled">
+            <li>
+              <a class="hvr" href="https://drive.google.com/open?id=1oo56yREd9s_Zmw_sfa8bJ3xJs6jE-eb3" target="_blank">ACP Portfolio Requirements</a>
+              <p class="list-graph">The ACP portfolio requirements allow students to reflect on their high school experiences through the lens of know, explore, plan, and go. This process helps students discover and develop a personalized plan for their life after high school.</p>
+            </li>
+            <li>
+              <a class="hvr" href="https://www.bls.gov/k12/" target="_blank">Bureau of Labor Statistics</a>
+              <p class="list-graph">The Bureau of Labor Statistics’ mission is to collect, analyze, and disseminate essential economic information to support public and private decision-making. As an independent statistical agency, BLS serves its diverse user communities by providing products and services that are objective, timely, accurate, and relevant.</p>
+            </li>
+            <li>
+              <a class="hvr" href="http://dwd.wi.gov/skillexplorer/brochure.pdf" target="_blank">Skill Explorer</a>
+              <p class="list-graph">Skill Explorer helps students identify transferable skills, career pathway opportunities, and training needs for occupations they are interested in pursuing.</p>
+            </li>
+            <li>
+              <a class="hvr" href="http://www.military.com/join-armed-forces/us-military-overview.html" target="_blank">U.S. Armed Forces Overview</a>
+              <p class="list-graph">This link gives background of the Armed Forces and what they have to offer to people interested in joining, as well as news and information for active military and veterans.</p>
+            </li>
+            <li>
+              <a class="hvr" href="https://jobcenterofwisconsin.com" target="_blank">Job Center of Wisconsin</a>
+              <p class="list-graph">This link allows employers and job seekers to search the state of Wisconsin for suitable employees and occupations.</p>
+            </li>
+            <li>
+              <a class="hvr" href="http://www.wtcsystem.edu/" target="_blank">Wisconsin Technical College System</a>
+              <p class="list-graph">The Wisconsin Technical College System develops individuals at every stage of their academic and work careers into experts and entrepreneurs who make an immediate, local economic impact.</p>
+            </li>
+            <li>
+              <a class="hvr" href="http://www.wisconsinsprivatecolleges.org" target="_blank">Wisconsin Private Colleges</a>
+              <p class="list-graph">Wisconsin's private, nonprofit colleges and universities working together for educational opportunity.</p>
+            </li>
+            <li>
+              <a class="hvr" href="https://www.wisconsin.edu" target="_blank">University of Wisconsin Colleges</a>
+              <p class="list-graph">The UW System is made up of 13 four-year universities, 13 freshman-sophomore UW Colleges campuses, and statewide UW-Extension. Together, these institutions are a tremendous academic, cultural, and economic resource for Wisconsin, the nation, and the world.</p>
+            </li>
+            <li>
+              <a class="hvr" href="https://sso.careercruising.com/page/openidconnect" target="_blank">Career Cruising</a>
+              <p class="list-graph">Career Cruising is an Internet-based career exploration and planning tool used by students in grades 6-12 to explore career and college options and develop a career plan. Through Career Cruising students will build self-awareness, learn about career possibilities and educational pathways, and create an actionable plan to achieve career, school and life goals.</p>
+            </li>
+          </ul>
+        </div><!-- /#sidebar -->
 
-            </div><!-- /#content -->
+      </div><!-- /#content -->
 
-            <?php include 'templates/footer.php'; ?>
+      <?php include 'templates/footer.php'; ?>
 
-        </div><!-- /.container-fluid -->
-        
-        <?php include 'templates/scripts.php'; ?>
-             
-    </body>
+    </div><!-- /.container-fluid -->
+    
+    <?php include 'templates/scripts.php'; ?>
+         
+  </body>
 </html>
