e50d16a1eb1b4e79694cee5248b9730c945dcad3
Cory Romdenne <cory@romdenne.com>
2018-05-21 12:15:33 -0500

    Add new career of the week, add photo to PI26 & ACP

--- pi26.php
+++ pi26.php
@@ -1,92 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Academic & Career Planning - Luxemburg-Casco School District">
+    <meta name="keywords" content="Luxemburg-Casco,Wisconsin,ACP,Academic,Career,Planning">
+    <meta name="author" content="Cory Romdenne">
     
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="Academic & Career Planning - Luxemburg-Casco School District">
-        <meta name="keywords" content="Luxemburg-Casco,Wisconsin,ACP,Academic,Career,Planning">
-        <meta name="author" content="Cory Romdenne">
-        
-        <title>P.I. 26 &amp; ACP – L-C ACP</title>
-        
-        <?php include 'templates/head.php'; ?>
-                    
-    </head>
+    <title>P.I. 26 &amp; ACP – L-C ACP</title>
     
-    <body>
+    <?php include 'templates/head.php'; ?>
+                  
+  </head>
+  
+  <body>
 
-        <div class="container-fluid">
-        
-            <?php include 'templates/mast-begin.php'; ?>
-                            <li><a href="index.php">Home</a></li>
-                            <li><a href="know.php">Know</a></li>
-                            <li><a href="explore.php">Explore</a></li>
-                            <li><a href="plan.php">Plan</a></li>
-                            <li><a href="go.php">Go</a></li>
-                            <li class="active"><a href="pi26.php">PI 26 &amp; ACP <span class="sr-only">(current)</span></a></li>
-                            <li><a href="parents.php">Parents</a></li>
-                            <li><a href="staff.php">Staff</a></li>
-                            <li><a href="partnerships.php">Partnerships</a></li>
-                <?php include 'templates/mast-end.php'; ?>
+    <div class="container-fluid">
+    
+      <?php include 'templates/mast-begin.php'; ?>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="know.php">Know</a></li>
+                <li><a href="explore.php">Explore</a></li>
+                <li><a href="plan.php">Plan</a></li>
+                <li><a href="go.php">Go</a></li>
+                <li class="active"><a href="pi26.php">PI 26 &amp; ACP <span class="sr-only">(current)</span></a></li>
+                <li><a href="parents.php">Parents</a></li>
+                <li><a href="staff.php">Staff</a></li>
+                <li><a href="partnerships.php">Partnerships</a></li>
+          <?php include 'templates/mast-end.php'; ?>
 
-            <div id="content" class="row row-eq-height">
-                <div id="pane-left" class="col-md-9">
-                    <h1>PI 26 &amp; ACP</h1>
-                    <img src="images/spear.png" width="100%" alt="spear header underline">
-                    <br><br>
-                    <p>
-                        Beginning with the 2017-2018 school year in the state of Wisconsin, every school board and district is required to provide academic and career planning <a class="hvr" href="https://docs.google.com/document/d/1_ujnYEKWyk3304RSOd3tybBftJF7z1rMQz1XkHtMric/edit" target="_blank">services to students in grades 6-12</a> enrolled in the district. In addition under PI 26, school boards and districts must meet the following requirements:
-                        <ul>
-                            <li>Develop a long-range plan with school district staff and community stakeholders.</li>
-                            <li>Publish the plan on the school district’s website.</li>
-                            <li>Review the plan yearly.</li>
-                        </ul>
-                    </p>
-                    
-                    <p class="graph-space">
-                        This plan must address:
-                        <ul>
-                            <li>Local, regional, and state market needs.</li>
-                            <li>Education and training requirements for occupations that will fill labor market needs.</li>
-                            <li>Process to engage parents regarding ACP services provided and opportunities to participate.</li>
-                            <li>Description of career and technical education (CTE) programming available, staff professional development for ACP delivery, and how school district will meet education for employment program requirements.</li>
-                            <li>Strategy to engage business, postsecondary education, and workforce development.</li>
-                        </ul>
-                    </p>
-                </div><!-- /#main -->
-                <div id="pane-right" class="col-md-3 sidebar">
-                    <h3>Resources</h3>
-                    <ul class="unstyled">
-                        <li>
-                            <a class="hvr" href="https://dpi.wi.gov/sites/default/files/imce/acp/pdf/pi26memo0108.pdf" target="_blank">PI 26</a>
-                            <p class="list-graph">PI 26 is a state administrative rule and the intent of the rule is outlined in this memorandum.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://dpi.wi.gov/acp" target="_blank">DPI ACP Resources</a>
-                            <p class="list-graph">The Wisconsin Department of Public Instruction has a variety of resources and guidelines for the ACP process.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://drive.google.com/open?id=1oo56yREd9s_Zmw_sfa8bJ3xJs6jE-eb3" target="_blank">ACP Portfolio Requirements</a>
-                            <p class="list-graph">The ACP portfolio requirements allow students to reflect on their high school experiences through the lens of know, explore, plan, and go. This process helps students discover and develop a personalized plan for their life after high school.</p>
-                        </li>
-                        <li>
-                            <a class="hvr" href="https://docs.google.com/document/d/1_ujnYEKWyk3304RSOd3tybBftJF7z1rMQz1XkHtMric" target="_blank">L-C Scope &amp; Sequence</a>
-                            <p class="list-graph">This document is a map of the learning activities that are being implemented in specific grade level/content areas.</p>
-                        </li>
-                          <li><a class="hvr" href="https://drive.google.com/file/d/1owZYj-tRmjF0wtHeui_zgAiZPOTRmuje/view?usp=sharing" target="_blank">Career Cruising Scope &amp; Sequence</a></li>
-                        <li><a class="hvr" href="https://docs.google.com/document/d/1upWDLHiFpYpqKbmtql42pmxRJ7sljT1xfszCL5ii0d4/edit?usp=sharing" target="_blank">L-C Strategic Plan (Spring '17)</a></li>
-                        <li><a class="hvr" href="https://docs.google.com/document/d/1ep2uxeeDnSMFZpjErTbtSGGBhU5YPLxUNiQrhM8JvRY/edit?usp=sharing" target="_blank">L-C ACP Multi-Year Plan</a></li>
-                    </ul>
-                </div><!-- /#sidebar -->
+      <div id="content" class="row row-eq-height">
+        <div id="pane-left" class="col-md-9">
+          <h1>PI 26 &amp; ACP</h1>
+          <img src="images/spear.png" width="100%" alt="spear header underline">
+          <br><br>
+          <img class="img-responsive img-thumbnail pull-right"
+               src="images/LCACP-CESA7share.JPG" width="300px">
+          <p>
+            Beginning with the 2017-2018 school year in the state of Wisconsin, every school board and district is required to provide academic and career planning <a class="hvr" href="https://docs.google.com/document/d/1_ujnYEKWyk3304RSOd3tybBftJF7z1rMQz1XkHtMric/edit" target="_blank">services to students in grades 6-12</a> enrolled in the district. In addition under PI 26, school boards and districts must meet the following requirements:
+            <ul>
+              <li>Develop a long-range plan with school district staff and community stakeholders.</li>
+              <li>Publish the plan on the school district’s website.</li>
+              <li>Review the plan yearly.</li>
+            </ul>
+          </p>
+          
+          <p class="graph-space">
+            This plan must address:
+            <ul>
+              <li>Local, regional, and state market needs.</li>
+              <li>Education and training requirements for occupations that will fill labor market needs.</li>
+              <li>Process to engage parents regarding ACP services provided and opportunities to participate.</li>
+              <li>Description of career and technical education (CTE) programming available, staff professional development for ACP delivery, and how school district will meet education for employment program requirements.</li>
+              <li>Strategy to engage business, postsecondary education, and workforce development.</li>
+            </ul>
+          </p>
+        </div><!-- /#main -->
+        <div id="pane-right" class="col-md-3 sidebar">
+          <h3>Resources</h3>
+          <ul class="unstyled">
+            <li>
+              <a class="hvr"
+                 href="https://dpi.wi.gov/sites/default/files/imce/acp/pdf/pi26memo0108.pdf"
+                 target="_blank">
+                PI 26
+              </a>
+              <p class="list-graph">
+                PI 26 is a state administrative rule and the intent of the rule is outlined in this memorandum.
+              </p>
+            </li>
+            <li>
+              <a class="hvr"
+                 href="https://dpi.wi.gov/acp"
+                 target="_blank">
+                DPI ACP Resources
+              </a>
+              <p class="list-graph">
+                The Wisconsin Department of Public Instruction has a variety of resources and guidelines for the ACP process.
+              </p>
+            </li>
+            <li>
+              <a class="hvr"
+                 href="https://drive.google.com/open?id=1oo56yREd9s_Zmw_sfa8bJ3xJs6jE-eb3"
+                 target="_blank">
+                ACP Portfolio Requirements
+              </a>
+              <p class="list-graph">
+                The ACP portfolio requirements allow students to reflect on their high school experiences through the lens of know, explore, plan, and go. This process helps students discover and develop a personalized plan for their life after high school.
+              </p>
+            </li>
+            <li>
+              <a class="hvr"
+                 href="https://docs.google.com/document/d/1_ujnYEKWyk3304RSOd3tybBftJF7z1rMQz1XkHtMric"
+                 target="_blank">
+                L-C Scope &amp; Sequence
+              </a>
+              <p class="list-graph">
+                This document is a map of the learning activities that are being implemented in specific grade level/content areas.
+              </p>
+            </li>
+            <li>
+              <a class="hvr"
+                 href="https://drive.google.com/file/d/1owZYj-tRmjF0wtHeui_zgAiZPOTRmuje/view?usp=sharing"
+                 target="_blank">
+                Career Cruising Scope &amp; Sequence
+              </a>
+            </li>
+            <li>
+              <a class="hvr"
+                 href="https://docs.google.com/document/d/1upWDLHiFpYpqKbmtql42pmxRJ7sljT1xfszCL5ii0d4/edit?usp=sharing"
+                 target="_blank">
+                L-C Strategic Plan (Spring '17)
+              </a>
+            </li>
+            <li>
+              <a class="hvr"
+                 href="https://docs.google.com/document/d/1ep2uxeeDnSMFZpjErTbtSGGBhU5YPLxUNiQrhM8JvRY/edit?usp=sharing"
+                 target="_blank">
+                L-C ACP Multi-Year Plan
+              </a>
+            </li>
+          </ul>
+        </div><!-- /#sidebar -->
 
-            </div><!-- /#content -->
+      </div><!-- /#content -->
 
-            <?php include 'templates/footer.php'; ?>
+      <?php include 'templates/footer.php'; ?>
 
-        </div><!-- /.container-fluid -->
-        
-        <?php include 'templates/scripts.php'; ?>
-             
-    </body>
+    </div><!-- /.container-fluid -->
+    
+    <?php include 'templates/scripts.php'; ?>
+           
+  </body>
 </html>
