a864e4bdb2dab045828525e758f12031f77e6a44
Cory Romdenne <cory@romdenne.com>
2020-02-27 11:35:25 -0600

    Add new career of the week

--- explore.php
+++ explore.php
@@ -72,6 +72,10 @@
                 <p>Each week students at the Intermediate School showcase a new and exciting career path for Spartans. Check out the YouTube videos below to see all of the careers the Intermediate School students have explored.
                 </p>
                 <a class="hvr" target="_blank"
+                   href="https://www.youtube.com/watch?v=TzZFUFnlpL8">
+                  Middle School Teacher
+                </a><br>
+                <a class="hvr" target="_blank"
                    href="https://www.youtube.com/watch?v=XUiZIp5cpH0">
                   Coin, Vending & Amusement Machine Service & Repair
                 </a><br>
@@ -121,7 +125,7 @@
                 </a><br>
               </div><!-- /.col-md-6 -->
               <div class="col-md-6">
-                <iframe width="100%" height="315" src="https://www.youtube.com/embed/XUiZIp5cpH0?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/TzZFUFnlpL8?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
               </div><!-- /.col-md-6 -->
             </div><!-- /.row -->
           </p>
